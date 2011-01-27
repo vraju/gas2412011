5c32f85ac2a46a2bb3580786f567d4fcdc1043f3
Bradley Dean <Bradley@TrueGeek.com>
2011-01-27 18:40:52 -0500

    Pushing new changes

--- product/nothinbutdotnetstore/tasks/startup/Startup.cs
+++ product/nothinbutdotnetstore/tasks/startup/Startup.cs
@@ -10,32 +10,34 @@ namespace nothinbutdotnetstore.tasks.startup
 {
     public class Startup
     {
+
+		private static Dictionary<Type, DependencyFactory> all_factories = new Dictionary<Type, DependencyFactory>();
         public static void run()
         {
-            var all_factories = new Dictionary<Type, DependencyFactory>();
-            Container.facade_resolver = () => new BasicDependencyContainer(
-                new BasicDependencyRegistry(all_factories));
+
+            Container.facade_resolver = () => new BasicDependencyContainer(new BasicDependencyRegistry(all_factories));
 
             //your code below here
-            var stub_set_of_commands = new StubSetOfCommands();
-            var default_command_registry = new DefaultCommandRegistry(stub_set_of_commands);
-            var default_front_controller = new DefaultFrontController(default_command_registry);
-            var stub_request_factory = new StubRequestFactory();
+            var default_command_registry = new DefaultCommandRegistry(new StubSetOfCommands());
 
             TemplateRegistry template_registry = new StubTemplateRegistry();
             Renderer renderer = new DefaultRenderer(template_registry);
             Catalog repository = new StubCatalog();
+			
+			add_factory<FrontController>(new DefaultFrontController(default_command_registry));
+			add_factory<RequestFactory>(new StubRequestFactory());
+			add_factory<CommandRegistry>(default_command_registry);
+			add_factory<TemplateRegistry>(template_registry);
+			add_factory<Renderer>(renderer);
+			add_factory<Catalog>(repository);
+        	add_factory<ViewMainDepartments>(new ViewMainDepartments(repository, renderer));
 
-            all_factories.Add(typeof(FrontController), new BasicDependencyFactory(() => default_front_controller));
-            all_factories.Add(typeof(RequestFactory), new BasicDependencyFactory(() => stub_request_factory));
-            all_factories.Add(typeof(CommandRegistry), new BasicDependencyFactory(() => default_command_registry));
-            all_factories.Add(typeof(TemplateRegistry), new BasicDependencyFactory(() => template_registry));
-            all_factories.Add(typeof(Renderer), new BasicDependencyFactory(() => renderer));
-            all_factories.Add(typeof(Catalog), new BasicDependencyFactory(() => repository));
-
-            all_factories.Add(typeof(ViewMainDepartments), new BasicDependencyFactory(() =>
-                                                                                          new ViewMainDepartments(
-                                                                                          repository, renderer)));
         }
+
+		private static void add_factory<TKey>(Object value)
+		{
+			all_factories.Add(typeof(TKey), new BasicDependencyFactory(() => value));
+		}
+
     }
 }
